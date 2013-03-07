2d3757e716e6e84dc34dc5b26fd4106dfd691463
nickaigi <nickaigi@gmail.com>
2013-03-08 01:48:11 +0300

    huge pep8 cleanup

--- core/models.py
+++ core/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
+
 class News(models.Model):
     """
     Model to hold a News Items
     """
-    user = models.ForeignKey(User,verbose_name = 'Reported by')
+    user = models.ForeignKey(User, verbose_name='Reported by')
     title = models.CharField(max_length=200)
     story = models.TextField(max_length=500)
-    timestamp = models.DateTimeField(blank=True,verbose_name='Date Published',null=True)
+    timestamp = models.DateTimeField(blank=True, verbose_name='Date Published', null=True)
     last_edited = models.DateTimeField(auto_now=True)
     is_active = models.BooleanField(default=False)
     slug = models.SlugField()
@@ -22,19 +23,19 @@ class News(models.Model):
     def __unicode__(self):
         return self.title
 
-    def save(self,*args,**kwargs):
+    def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
-        super(News,self).save(*args,**kwargs)
+        super(News, self).save(*args, **kwargs)
 
 
 class Tag(models.Model):
     """
     Model for Tags associated with Stories
     """
-    user = models.ForeignKey(User,verbose_name='Created by')
-    news = models.ForeignKey(News,verbose_name='Tagged News')
+    user = models.ForeignKey(User, verbose_name='Created by')
+    news = models.ForeignKey(News, verbose_name='Tagged News')
     title = models.CharField(max_length=50)
-    timestamp = models.DateTimeField(blank=True,verbose_name='Date Created')
+    timestamp = models.DateTimeField(blank=True, verbose_name='Date Created')
     last_edited = models.DateTimeField(auto_now=True)
 
     class Meta:
