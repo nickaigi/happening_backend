00b6aa11a407b2c206003b5af6fb16a66723721f
nickaigi <nickaigi@gmail.com>
2013-04-17 00:08:23 +0300

    set up bootstrap, show first news item

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from core.models import News, Tag 
 
 def home(request):
     """
     Home page for the happening app
     """
-    return render_to_response('home.html', {}, context_instance=RequestContext(request))
+    news = News.objects.filter(is_active=True)
+    return render_to_response('home.html', {'news': news}, context_instance=RequestContext(request))
