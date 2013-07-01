ff7a9904eaf88974e18a947c7fa8bb45f5786636
Nickson Kaigi <nickaigi@gmail.com>
2013-07-01 20:17:14 +0300

    started writing tests

--- core/tests.py
+++ core/tests.py
@@ -1,16 +1,43 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class NewsTest(LiveServerTestCase):
+    '''
+    Test class for News model
+    '''
+    fixtures = ['initial_data.yaml']
+
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_create_news_via_admin_site(self):
+        self.browser.get(self.live_server_url + '/admin/')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Django administration', body.text)
+        
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('admin')
+        password_field.send_keys(Keys.RETURN)
+
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+        news_item_links = self.browser.find_elements_by_link_text('News Items')
+        self.assertEquals(len(news_item_links), 2 )
+        
+
+
+        self.fail('todo: Finish tests')
+
+
